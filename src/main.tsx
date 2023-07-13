620fb61c02cb17aadf02330ad9b4d20028df275f
B Vinothini <2000093505@hexaware.com>
2023-07-13 17:55:56 +0530

    Vehicle habitation and simulation screen design implemented

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Index from './components/vehicle-habitation/index.tsx'
+import VehicleSimulation from './components/vehicle-simulation/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/vehicle-habitation",
+    element: <Index />,
+  },
+  {
+    path: "/vehicle-simulation",
+    element: <VehicleSimulation />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router><App /></Router>
+    {/* <Router><App />
+    
+    </Router> */}
+     <RouterProvider router={router} />
   </React.StrictMode>,
+  
 )
