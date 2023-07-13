ac401fb88cdb0297e0323c386ad6f8663bed3032
Navnath Auti <2000080160@hexaware.com>
2023-07-13 20:58:52 +0530

    updated code

--- src/components/simulation/simulation.tsx
+++ src/components/simulation/simulation.tsx
@@ -1,5 +1,6 @@
 import "./simulation.css";
 import { contenthome } from "../../assets/images";
+import { Link } from "react-router-dom";
 
 const VehicleSimulation = () => {
   return (
@@ -8,12 +9,18 @@ const VehicleSimulation = () => {
         <img src={contenthome} className="content-home" />
       </div>
       <div className="p-5">
-        <span className="h3">Simulation en ligne</span>
+        <span className="h3">
+          {/* Simulation en ligne */}
+          Online simulation
+        </span>
       </div>
 
       <div className="p-5">
         <div className="row p-2">
-          <span className="h4">Remplissez votre numero de chassis</span>
+          <span className="h4">
+            {/* Remplissez votre numero de chassis */}
+            Enter your chassis number
+          </span>
           <div className="p-5">
             <input
               className="form-control"
@@ -28,13 +35,22 @@ const VehicleSimulation = () => {
       <div style={{ float: "right" }}>
         <div className="p-2">
           <button disabled type="button" className="btn btn-primary">
-            Suivant
+            {/* Suivant */}
+            Next
           </button>
         </div>
         <div className="p-2">
-          <button type="button" className="btn btn-primary">
+          <Link
+            to="/details"
+            style={{ width: "200px", background: "lightgreen" }}
+            className="btn btn-primary col-3 mb-2 order-1"
+          >
+            Without chassis number
+          </Link>
+          {/* <button type="button" className="btn btn-primary">
             Sans numero de chassis
-          </button>
+            Without chassis number
+          </button> */}
         </div>
       </div>
     </div>
