319f09e25204eb213be6042b834cf525371eda33
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-07-13 09:33:56 +0530

    commit the changes

--- src/components/Details/test.js
+++ src/components/Details/test.js
@@ -16,16 +16,17 @@ const Details = () => {
     return (
         <div>
             <Header />
-            <div className='details-page d-flex'>
-                {/* details-page */}
-
+            <div className='container'>
+            {/* details-page */}
+            <div className="row row-cols-3">
+                <div className="col">Column</div>
                 <div className={open ? 'popup' : 'popup-hide'}>
                     <i className="fa fa-times-circle" onClick={handleClick} aria-hidden="true"></i>
                     <h2>Valeur facture</h2>
                     <p>Il est possible de retrouver cette information sur la facture, le bon de commande ou le contrat de vente qui vous a été remis lors de l'achat de votre voiture.</p>
                     <p>Si vous n'avez plus ces documents, vous pouvez vérifier vos relevés de compte pour retrouver le montant et la date de paiement.</p>
                 </div>
-                <div className={open ? 'details-info-blur' : 'details-info'}>
+                <div className={open ? 'details-info-blur' : ''}>
                     <Link to="/">
                         <button className='back-button'>
                             <i className="fa fa-chevron-left" aria-hidden="true"></i>
@@ -38,29 +39,50 @@ const Details = () => {
                     <form>
                         <div className='details-input' >
                             <div> Carburant </div>
-                            {toggle ?
-                                <select className="form-select form-select-lg dropdown" id="fuels" >
-                                    {
-                                        Vehicle_Details.Fuels
-                                            .length > 0 &&
-                                        Vehicle_Details.Fuels?.map(
-                                            (item, index) => {
-                                                return (
-                                                    <option
-                                                        key={index}
-                                                        value={item.value} >
-                                                        {item.label}
-                                                    </option>
-                                                )
-                                            }
-                                        )}
-                                </select>
+                            {toggle ? 
+                            <select className="form-select form-select-lg" id="fuels" >
+                                {
+                                    Vehicle_Details.Fuels
+                                        .length > 0 &&
+                                    Vehicle_Details.Fuels?.map(
+                                        (item, index) => {
+                                            return (
+                                                <option
+                                                    key={index}
+                                                    value={item.value} >
+                                                    {item.label}
+                                                </option>
+                                            )
+                                        }
+                                    )}
+                            </select>
                                 : <span> Esscence </span>}
                         </div>
 
+                        {toggle ? <div className='details-input' >
+                            <label htmlFor=""> Annee de construction </label>
+                            <select className="form-select form-select-lg" id="fuels" >
+                                {
+                                    Vehicle_Details.Fuels
+                                        .length > 0 &&
+                                    Vehicle_Details.Fuels?.map(
+                                        (item, index) => {
+                                            return (
+                                                <option
+                                                    key={index}
+                                                    value={item.value} >
+                                                    {item.label}
+                                                </option>
+                                            )
+                                        }
+                                    )}
+                            </select>
+                        </div>
+                            : <></>}
+
                         <div className='details-input' >
-                            <div  > Marque </div>
-                            {toggle ? <select className="form-select form-select-lg dropdown" id="fuels" >
+                            <label htmlFor="" > Marque </label>
+                            {toggle ? <select className="form-select form-select-lg" id="fuels" >
                                 {
                                     Vehicle_Details.Fuels
                                         .length > 0 &&
@@ -79,8 +101,8 @@ const Details = () => {
                                 : <span> Volkswagen </span>}
                         </div>
                         <div className='details-input' >
-                            <div > Modéle </div>
-                            {toggle ? <select className="form-select form-select-lg dropdown" id="fuels" >
+                            <label htmlFor=""> Modéle </label>
+                            {toggle ? <select className="form-select form-select-lg" id="fuels" >
                                 {
                                     Vehicle_Details.Fuels
                                         .length > 0 &&
@@ -99,8 +121,8 @@ const Details = () => {
                                 : <span> Golf Vill </span>}
                         </div>
                         <div className='details-input' >
-                            <div > Type </div>
-                            {toggle ? <select className="form-select form-select-lg dropdown" id="fuels" >
+                            <label htmlFor="" > Type </label>
+                            {toggle ? <select className="form-select form-select-lg" id="fuels" >
                                 {
                                     Vehicle_Details.Fuels
                                         .length > 0 &&
@@ -118,29 +140,9 @@ const Details = () => {
                             </select>
                                 : <span > tsi 110 </span>}
                         </div>
-                        {toggle ? <></>
-                            : <div className='details-input' >
-                                <div > Annee de construction </div>
-                                <select className="form-select form-select-lg dropdown" id="fuels" >
-                                    {
-                                        Vehicle_Details.Fuels
-                                            .length > 0 &&
-                                        Vehicle_Details.Fuels?.map(
-                                            (item, index) => {
-                                                return (
-                                                    <option
-                                                        key={index}
-                                                        value={item.value} >
-                                                        {item.label}
-                                                    </option>
-                                                )
-                                            }
-                                        )}
-                                </select>
-                            </div>}
-                        {toggle ? <div className='details-input' >
-                            <div> Première immatriculation </div>
-                            <select className="form-select form-select-sm dropdown" id="cars" required>
+                        <div className='details-input' >
+                            <label htmlFor="" > Première immatriculation </label>
+                            <select className="form-select form-select-sm" id="cars" required>
                                 {
                                     Vehicle_Details.Years
                                         .length > 0 &&
@@ -157,38 +159,31 @@ const Details = () => {
                                     )}
                             </select>
                         </div>
-                            : <></>}
                         <div className='details-input' >
-                            <div className='info-label'>
+                            <label htmlFor="" className='info-label'>
                                 Valeur facture(tvac)
                                 <i className="fa fa-info info-icon" onClick={handleClick} aria-hidden="true"> </i>
-                                <div> remises comprises (hors reprise) </div>
-                            </div>
+                                <span> remises comprises (hors reprise) </span>
+                            </label>
 
                             <div className="">
-                                <span className='euro'>&euro;</span>
-                                <input type="number" min="2000" max='157300' className='amount dropdown' placeholder='0,00' />
-
+                                <span>&euro;</span>
+                                <input type="number" min="2000" max='157300' className='amount' placeholder='0,00' />
                             </div>
                         </div>
                         < div className='details-input' >
-                            <div> Date d’achat de votre voiture</div>
-                            <input type="month" className='month dropdown' placeholder='mm/aaaa' min={FormatToday} max={FormatToday} required />
+                            <label htmlFor="" > Date d’achat de votre voiture</label>
+                            < input type="month" className='month' placeholder='mm/aaaa' min={FormatToday} max={FormatToday} required />
                         </div>
                         < div className="details-button" >
                             <button type="submit" className='btn btn-lg'>Suivant</button>
                         </div>
                     </form>
                 </div>
+                
+            </div>
             </div>
         </div>
-        // <div className="container">
-        //     <div className="row">
-        //         <div className="col-md-4">.col-md-8</div>
-        //         <div className="col-md-4">.col-md-8</div>
-        //         <div className="col-6 col-md-4">.col-6 .col-md-4</div>
-        //     </div>
-        // </div>
     )
 }
 
