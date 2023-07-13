929dcdccbe7ccd504cfb50c8f076754b2d4492d0
Navnath Auti <2000080160@hexaware.com>
2023-07-13 19:37:16 +0530

    updated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Home from "./components/home/home";
 import Header from "./components/common/header/header";
 import Dashboard from "./components/dashboard/dashboard";
-import "bootstrap/dist/css/bootstrap.css";
 import { Routes, Route } from "react-router-dom";
 
 import { RouteLinks } from "./constants";
--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -4,5 +4,6 @@ import assurance_auto from "./assurance_auto.png";
 import habitation from "./habitation.png";
 import headerlogo from "./headerlogo.png";
 import nngroup from "./nngroup.png";
+import rgfInsurance from "./rgfinsurances.png";
 
-export { logo, contenthome, assurance_auto, habitation, headerlogo, nngroup };
+export { logo, contenthome, assurance_auto, habitation, headerlogo, nngroup, rgfInsurance };
