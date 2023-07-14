fe9d8bf44fa535c86e3c5ab6640a42cc4f9ca386
hxvarisn <hxvarisn@domesticandgeneral.com>
2023-07-14 10:58:30 +0530

    First test commit nilesh

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,7 +1,7 @@
 import "./dashboard.css";
 
 const Dashboard = () => {
-  return <div>Hello Dashboard!</div>;
+  return <div>Hello Dashboard! Nilesh commit test</div>;
 };
 
 export default Dashboard;
