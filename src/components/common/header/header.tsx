a89bdc7913834540b79648ee38a9a1e8bf7bfd3d
Damini Sharma <1000064595@hexaware.com>
2023-07-18 16:48:39 +0530

    fix header length

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -4,7 +4,7 @@ import { headerlogo, nngroup } from "../../../assets/images";
 const Header = () => {
   return (
     // <div className="container w-50">
-    <div className="container mx-auto" style={{ width: "500px" }}>
+    <div className="container mx-auto" style={{ width: "470px" }}>
       <nav className="navbar navbar-light">
         <a className="navbar-brand" href="/">
           <img
