279ebf6c1fc111ed165ab966fb74a9b496398334
Damini Sharma <1000064595@hexaware.com>
2023-07-19 11:00:18 +0530

    Fixed position of header and pages alignment

--- src/components/simulation/simulation.tsx
+++ src/components/simulation/simulation.tsx
@@ -19,119 +19,121 @@ const VehicleSimulation = () => {
 
   return (
     <div>
-      <div className="container mx-auto">
-        <div className={open ? "popup" : "popup-hide"}>
-          <i
-            className="fa fa-times-circle"
-            onClick={handleClick}
-            aria-hidden="true"
-          ></i>
-          <h4>
-            Le numéro de chassis
-            <hr />{" "}
-          </h4>
-          <p>
-            7 Le numéro de chassis Ce numéro se trouve généralement à plusieurs
-            endroits :
-          </p>
-          <p>
-            Sur la planche de bord côté conducteur – vous pouvez le voir à
-            travers le pare-brise depuis l’extérieur de la voiture.
-          </p>
-          <p>
-            Sur la porte côté conducteur, vérifiez l’autocollant qui contient
-            des informations sur la voiture, y compris le numéro de chassis.
-          </p>{" "}
-          <p>
-            {" "}
-            Dans le compartiment moteur – ce numéro peut se trouver sur le bloc
-            moteur ou d’autres pièces du moteur.{" "}
-          </p>
-          <p>
-            {" "}
-            Sur les documents d’immatriculation de la voiture – généralement sur
-            le certificat d’immatriculation du véhicule.
-          </p>
-          <p>
-            Sur vos documents d’assurance – généralement sur votre carte ou
-            police d’assurance.
-          </p>
-        </div>
-        <div className={open ? "simulation-info-blur" : "simulation-info"}>
-          <div>
-            <img src={policybanner} className="content-home" />
-          </div>
-          <div className="p-3 container">
-            <span className="h4">
+      <div className="col-md-6 offset-md-3">
+        <div className="container mx-auto">
+          <div className={open ? "popup" : "popup-hide"}>
+            <i
+              className="fa fa-times-circle"
+              onClick={handleClick}
+              aria-hidden="true"
+            ></i>
+            <h4>
+              Le numéro de chassis
+              <hr />{" "}
+            </h4>
+            <p>
+              7 Le numéro de chassis Ce numéro se trouve généralement à
+              plusieurs endroits :
+            </p>
+            <p>
+              Sur la planche de bord côté conducteur – vous pouvez le voir à
+              travers le pare-brise depuis l’extérieur de la voiture.
+            </p>
+            <p>
+              Sur la porte côté conducteur, vérifiez l’autocollant qui contient
+              des informations sur la voiture, y compris le numéro de chassis.
+            </p>{" "}
+            <p>
               {" "}
-              {/* Simulation en ligne */}
-              Online simulation
-            </span>
-            <hr className="horizontal-line" />
+              Dans le compartiment moteur – ce numéro peut se trouver sur le
+              bloc moteur ou d’autres pièces du moteur.{" "}
+            </p>
+            <p>
+              {" "}
+              Sur les documents d’immatriculation de la voiture – généralement
+              sur le certificat d’immatriculation du véhicule.
+            </p>
+            <p>
+              Sur vos documents d’assurance – généralement sur votre carte ou
+              police d’assurance.
+            </p>
           </div>
+          <div className={open ? "simulation-info-blur" : "simulation-info"}>
+            <div>
+              <img src={policybanner} className="content-home" />
+            </div>
+            <div className="p-3 container">
+              <span className="h4">
+                {" "}
+                {/* Simulation en ligne */}
+                Online simulation
+              </span>
+              <hr className="horizontal-line" />
+            </div>
 
-          <div className="p-3 container">
-            <div className="row p-2 d-flex flex-column">
-              <div className="h5 col-12 info-span">
-                {/* Remplissez votre numero de chassis */}
-                Enter your chassis number
-                <i
-                  className="fa fa-info info-icon"
-                  onClick={handleClick}
-                  aria-hidden="true"
-                >
-                  {" "}
-                </i>
+            <div className="p-3 container">
+              <div className="row p-2 d-flex flex-column">
+                <div className="h5 col-12 info-span">
+                  {/* Remplissez votre numero de chassis */}
+                  Enter your chassis number
+                  <i
+                    className="fa fa-info info-icon"
+                    onClick={handleClick}
+                    aria-hidden="true"
+                  >
+                    {" "}
+                  </i>
+                </div>
+                <div className="p-2">
+                  <input
+                    className="form-control vehicleText"
+                    type="text"
+                    width="100"
+                    placeholder="ABCDEFG1234567890"
+                    value={vNumber}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+            </div>
+            <div style={{ float: "right" }}>
+              <div className="p-3 d-flex justify-content-end">
+                {vNumber ? (
+                  <>
+                    <Link to="/Car/YourCarDetails">
+                      <button
+                        type="button"
+                        disabled={!vNumber}
+                        className="btn btn-sm p-2"
+                      >
+                        Next
+                      </button>
+                    </Link>
+                  </>
+                ) : (
+                  <button
+                    type="button"
+                    disabled={!vNumber}
+                    className="btn btn-sm p-2"
+                  >
+                    {/* Suivant */}
+                    Next
+                  </button>
+                )}
               </div>
               <div className="p-2">
-                <input
-                  className="form-control vehicleText"
-                  type="text"
-                  width="100"
-                  placeholder="ABCDEFG1234567890"
-                  value={vNumber}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
-          </div>
-          <div style={{ float: "right" }}>
-            <div className="p-3 d-flex justify-content-end">
-              {vNumber ? (
-                <>
-                  <Link to="/Car/YourCarDetails">
-                    <button
-                      type="button"
-                      disabled={!vNumber}
-                      className="btn btn-sm p-2"
-                    >
-                      Next
-                    </button>
-                  </Link>
-                </>
-              ) : (
-                <button
-                  type="button"
-                  disabled={!vNumber}
-                  className="btn btn-sm p-2"
+                <Link
+                  to="/Car/YourCar"
+                  style={{ width: "200px" }}
+                  className="btn btn-outline col-3 mb-2 order-1 p-2"
                 >
-                  {/* Suivant */}
-                  Next
-                </button>
-              )}
-            </div>
-            <div className="p-2">
-              <Link
-                to="/Car/YourCar"
-                style={{ width: "200px" }}
-                className="btn btn-outline col-3 mb-2 order-1 p-2"
-              >
-                Without chassis number
-              </Link>
-              {/* <button type="button" className="btn btn-primary">
+                  Without chassis number
+                </Link>
+                {/* <button type="button" className="btn btn-primary">
             Sans numero de chassis
             Without chassis number
           </button> */}
+              </div>
             </div>
           </div>
         </div>
